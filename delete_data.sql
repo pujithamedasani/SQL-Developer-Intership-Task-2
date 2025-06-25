d0678e9530d507e1af4ea11a319ce8ed958234b5
Pujitha Medasani <mpujitha676@gmail.com>
2025-06-25 12:53:46 +0530

    Create delete_data.sql

---
+++ delete_data.sql
@@ -0,0 +1,6 @@
+--The DELETE statement is used to delete existing records in a table.
+--Syntax:
+DELETE FROM table_name WHERE condition;
+
+-- Delete a reservation
+DELETE FROM Reservations WHERE ReservationID = 1;
